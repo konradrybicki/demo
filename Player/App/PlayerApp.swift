98e4b598475e61c5fd58cfff89b72278610a23f0
Konrad Rybicki <konrad.rybicki.dev@gmail.com>
2024-11-25 15:51:58 +0100

    Setup

---
+++ Player/App/PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerApp.swift
+//  Player
+//
+//  Created by Konrad Rybicki on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
